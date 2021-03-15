3cd1e7f29b9dd26fa4d564b8840ad466fde739a9
DC-TGP-017 <rajsekhar@dualcube.com>
2021-03-15 13:06:44 +0530

    Plugin updated with version 1.2.3

--- wcmp-advance-shipping.php
+++ wcmp-advance-shipping.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://wc-marketplace.com/
  * Description: A free Addon Bridging WC Marketplace and WC Table Rate Shipping.
  * Author: WC Marketplace
- * Version: 1.1.2
+ * Version: 1.1.3
  * Author URI: https://wc-marketplace.com/
  * 
  * Text Domain: wcmp-advance-shipping
